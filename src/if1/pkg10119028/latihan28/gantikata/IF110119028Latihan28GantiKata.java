af8641025c39692d6474b58b1c2c3952b43d392b
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-10-30 11:35:34 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan28/gantikata/IF110119028Latihan28GantiKata.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan28.gantikata;
+import java.util.Scanner;
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk mengganti sebuah kata di dalam kalimat
+ */
+public class IF110119028Latihan28GantiKata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main (String[] args) {
+        String hasil = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("====Program Mengganti Kata====\n");
+        System.out.print("Masukkan Kalimat : ");
+        String kalimat = scanner.nextLine();
+        System.out.print("Ganti Kata : ");
+        String kata_lama = scanner.nextLine();
+        System.out.print("Menjadi Kata : ");
+        String kata_baru = scanner.nextLine();
+        String[] perkata = kalimat.split(" ");
+        for (String kata : perkata) {
+            if (kata.equalsIgnoreCase(kata_lama)) {
+                hasil += kata_baru + " ";
+            }
+            else {
+                hasil += kata + " ";
+            }
+        }
+        System.out.println("Kalimat Baru : "+hasil.trim());
+    }
+}
